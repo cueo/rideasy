0e49f5a9aaab1a4921018c1c5521b73c8aed52a9
cueo <mohit.ritanil@gmail.com>
2018-12-08 15:40:46 +0530

    Initial commit

---
+++ src/main/kotlin/com/cueball/rideasy/RideasyApplication.kt
@@ -0,0 +1,11 @@
+package com.cueball.rideasy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RideasyApplication
+
+fun main(args: Array<String>) {
+    runApplication<RideasyApplication>(*args)
+}
