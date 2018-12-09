01d8a437f5da6f16f63863669077f4a2cdabaf1a
cueo <mohit.ritanil@gmail.com>
2018-12-09 23:13:00 +0530

    Update UT

--- src/test/kotlin/com/cueball/rideasy/api/UberServiceTest.kt
+++ src/test/kotlin/com/cueball/rideasy/api/UberServiceTest.kt
@@ -1,21 +1,41 @@
 package com.cueball.rideasy.api
 
-import org.junit.jupiter.api.Assertions.*
+import com.cueball.rideasy.data.UberRide
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 /**
- * @author mmayank
+ * @author cueo
  */
 internal class UberServiceTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    private lateinit var uber: UberService
+    private val startLatitude: Float = 12.966172F
+    private val startLongitude: Float = 77.714975F
+    private val endLatitude: Float = 12.926762F
+    private val endLongitude: Float = 77.686592F
+
+    @BeforeEach
     fun setUp() {
+        uber = UberService()
     }
 
-    @org.junit.jupiter.api.Test
-    fun initRidesService() {
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     fun getPriceEstimate() {
+        val prices = uber.getPriceEstimates(startLatitude, startLongitude, endLatitude, endLongitude)
+        assertTrue(prices!!.stream().anyMatch { item -> "UberGo" == item.displayName })
+        assertTrue(prices.stream().anyMatch { item -> "Pool" == item.displayName })
+        assertTrue(prices.stream().anyMatch { item -> "UberXL" == item.displayName })
+        assertTrue(prices.stream().anyMatch { item -> "Premier" == item.displayName })
+        assertTrue(prices.stream().anyMatch { item -> "Pool Airport" == item.displayName })
+    }
+
+    @Test
+    fun getPickupTimeEstimate() {
+        var pickupTimeEstimate = uber.getPickupTimeEstimate(startLatitude, startLongitude, UberRide.POOL)
+        assertTrue(pickupTimeEstimate!! > 0)
+        pickupTimeEstimate = uber.getPickupTimeEstimate(startLatitude, startLongitude, UberRide.GO)
+        assertTrue(pickupTimeEstimate!! > 0)
     }
 }
